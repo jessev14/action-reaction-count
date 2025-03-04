82fcbf4d508ca6a1538d9a18ff5c65b20b10b729
jessev14 <jessevo321@gmail.com>
2025-03-04 15:32:13 -0500

    add actions to core sheet

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ const lg = x => console.log(x);
 
 Hooks.once('init', () => {
     for (const activityType of Object.values(CONFIG.DND5E.activityActivationTypes)) activityType.scalar = true;
-    
+
     CONFIG.DND5E.advancementTypes.ActionMaximum = {
         documentClass: ActionMaximum,
         validItemTypes: new Set(['class'])
@@ -63,6 +63,62 @@ Hooks.on('dnd5e.preUseActivity', (activity, usageConfig, dialogConfig, messageCo
 
     return actor.setFlag(moduleID, `${itemActionType}.count`, actorActionCount - actionCost);
 
+});
+
+Hooks.on('renderActorSheet5eCharacter2', (app, [html], appData) => {
+    const { actor } = app;
+
+    const actionsBox = document.createElement('filigree-box');
+    actionsBox.classList.add('saves', 'actions');
+    actionsBox.innerHTML = `
+        <h3>
+            <span class="roboto-upper">Actions</span>
+        </h3>
+        <ul>
+            <li class="action">
+                <label>Normal</label>
+                <div class="count-inputs">
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.normal.count"
+                        value="${actor.getFlag(moduleID, 'normal.count') || 0}" placeholder="0" />
+                    <span class="sep">/</span>
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.normal.max"
+                        value="${actor.getFlag(moduleID, 'normal.max') || 3}" placeholder="0" ${actor.type === 'character' ? 'disabled' : ''} />
+                </div>
+            </li>
+            <li class="action">
+                <label>Reaction</label>
+                <div class="count-inputs">
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.reaction.count"
+                        value="${actor.getFlag(moduleID, 'reaction.count') || 0}" placeholder="0" />
+                    <span class="sep">/</span>
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.reaction.max"
+                        value="${actor.getFlag(moduleID, 'reaction.max') || 1}" placeholder="0" ${actor.type === 'character' ? 'disabled' : ''} />
+                </div>
+            </li>
+            <li class="action">
+                <label>Attack</label>
+                <div class="count-inputs">
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.attack.count"
+                        value="${actor.getFlag(moduleID, 'attack.count') || 0}" placeholder="0" />
+                    <span class="sep">/</span>
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.attack.max"
+                        value="${actor.getFlag(moduleID, 'attack.max') || 0}" placeholder="0" ${actor.type === 'character' ? 'disabled' : ''} />                </div>
+            </li>
+            <li class="action">
+                <label>Magic</label>
+                <div class="count-inputs">
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.spell.count"
+                        value="${actor.getFlag(moduleID, 'spell.count') || 0}" placeholder="0" />
+                    <span class="sep">/</span>
+                    <input type="number" min="0" step="1" name="flags.${moduleID}.spell.max"
+                        value="${actor.getFlag(moduleID, 'spell.max') || 0}" placeholder="0" ${actor.type === 'character' ? 'disabled' : ''} />
+                </div>
+            </li>
+        </ul>
+    `;
+
+    const savesBox = html.querySelector('filigree-box.saves');
+    savesBox.before(actionsBox);
 });
 
 Hooks.on('tidy5e-sheet.renderActorSheet', (app, html, appData) => {
